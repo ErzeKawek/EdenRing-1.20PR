ef609ed065d8a6fa14426ead3c7132850e956b29
ErzeKawek <130212655+ErzeKawek@users.noreply.github.com>
2024-05-15 19:42:00 +0700

    Remove Comment From CBCLSetiings

--- src/main/java/paulevs/edenring/world/CBCLBiomeSettings.java
+++ src/main/java/paulevs/edenring/world/CBCLBiomeSettings.java
@@ -4,6 +4,6 @@ import org.betterx.bclib.api.v2.levelgen.biomes.BCLBiomeSettings;
 
 public class CBCLBiomeSettings extends BCLBiomeSettings {
     public CBCLBiomeSettings() {
-        super(); // Call the protected constructor of the superclass
+        super();
     }
 }
